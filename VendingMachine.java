08b986f6d4292405627af67471ad1822769eb027
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-12 20:30:06 -0500

    Steps 3 and 4

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class VendingMachine {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         String[] items = {"Lays", "Doritos", "Pop Tarts", "Twinkies", "Coke", "Pepsi", "Gatorade", "Water"};
         int[] prices = {2, 2, 3, 2, 3, 3, 2, 1};
 
@@ -7,5 +11,8 @@ public class VendingMachine {
             System.out.println(items[i] + " $" + prices[i]);
         }
         
+        System.out.println("Hello, enter an item you want to buy. Only $1 bills are accepted.");
+        System.out.println("What snack would you want to buy?");
+        String chosenFood = in.next();
     }
 }
