145ca685b30fd7cc7f0ee0841e45db750bdc191a
mariop410 <mpanagrosso410@gmail.com>
2026-02-18 16:02:58 -0500

    added user being able to insert only 1$ dollar bills into the machine and once they selected their choice and how much money they will be printed a message giving themn their snack choice and change (steps 5 and 6)

--- VendingMachine.java
+++ VendingMachine.java
@@ -11,8 +11,26 @@ public class VendingMachine {
             System.out.println(items[i] + " $" + prices[i]);
         }
         
-        System.out.println("Hello, enter an item you want to buy. Only $1 bills are accepted.");
         System.out.println("What snack would you want to buy?");
+        String snack = in.next();
+
+        for (int i = 0; i < items.length; i++) {
+            if (snack.equals(items[i])) {
+                System.out.println("Insert only 1$ bills.  How many would you like to insert?");
+                int dollars = in.nextInt();
+
+                if (dollars < prices[i]) {
+                    System.out.println("Not enough money");
+                }
+                else {
+                    System.out.println("Here is your " + items[i]);
+                    System.out.println("Here is you change: $" + (dollars-prices[i]));
+                }
+            }
+        }
+    
+        
+       
         String chosenFood = in.next();
     }
 }
