35f153399767afe875dbc9c44bf5f953567d2d54
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-18 20:11:37 -0500

    Added if statement and farewell message (steps 7 & 8)

--- VendingMachine.java
+++ VendingMachine.java
@@ -13,24 +13,33 @@ public class VendingMachine {
         
         System.out.println("What snack would you want to buy?");
         String snack = in.next();
-
+        
         for (int i = 0; i < items.length; i++) {
             if (snack.equals(items[i])) {
                 System.out.println("Insert only 1$ bills.  How many would you like to insert?");
                 int dollars = in.nextInt();
 
+
                 if (dollars < prices[i]) {
                     System.out.println("Not enough money");
                 }
-                else {
-                    System.out.println("Here is your " + items[i]);
-                    System.out.println("Here is you change: $" + (dollars-prices[i]));
+
+                if (dollars >= prices[i]) {
+                    System.out.print("Do you want to confirm this purchace? (Y to confirm) ");
+                    String decision = in.next();
+
+                    if ((decision.equals("Y") || decision.equals("y"))) {
+                        System.out.println("Here is your " + items[i]);
+                        System.out.println("Here is you change: $" + (dollars-prices[i]));
+
+                        System.out.println("Enjoy your snack!" );
+                    } else {
+                    System.out.println("Here is your money back: " + dollars);
+                    }
                 }
             }
         }
-    
         
-       
-        String chosenFood = in.next();
+       System.out.println("Have a nice day! ");
     }
 }
